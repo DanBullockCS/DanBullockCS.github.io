4866e6437d84fba6034988cd7305b7f822d95d93
Daniel Bullock <BusinessDaniel@live.com>
2020-05-29 21:56:57 -0400

    Flipped some buttons

--- scripts/cardflip.js
+++ scripts/cardflip.js
@@ -1,8 +1,9 @@
+// Flips the Project cards back and forth via a button
 var cardDiv;
 
 function flipCard(cardID) {
     cardDiv = document.getElementById(cardID);
-    cardDiv.style.transition = '0.8s';
+    cardDiv.style.transition = '0.5s';
     cardDiv.style.transform = 'rotateY(180deg)';
     cardDiv.childNodes[1].style.display = 'none';
     cardDiv.childNodes[3].style.display = 'block';
@@ -10,8 +11,8 @@ function flipCard(cardID) {
 
 function flipCardBack(cardID) {
     cardDiv = document.getElementById(cardID);
-    cardDiv.style.transition = '0.8s';
-    cardDiv.style.transform = 'rotateY(0deg)';
+    cardDiv.style.transition = '0.5s';
+    cardDiv.style.transform = 'rotateY(360deg)';
     cardDiv.childNodes[1].style.display = 'block';
     cardDiv.childNodes[3].style.display = 'none';
 }
