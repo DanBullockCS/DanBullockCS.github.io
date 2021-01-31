13e8faabcbe6143c7ee28a3577c084e0553fecd5
Dan <businessdaniel@live.com>
2021-01-31 15:08:12 -0500

    Added WallpaperPix to randomproject.js

--- scripts/randomProject.js
+++ scripts/randomProject.js
@@ -1,7 +1,7 @@
 // Select a random live project when you click the live project SVG
 function showRandomProject() {
     let Link = document.getElementById("svg-link");
-    let numProjects = 6;
+    const numProjects = 7;
     
     switch (Math.floor(Math.random() * numProjects)) {
         case 0:
@@ -21,6 +21,9 @@ function showRandomProject() {
             break;
         case 5:
             Link.href = "PaintSplatterTechDemo";
+            break;
+        case 6:
+            Link.href = "WallpaperPix";
             break;
     }
 }
