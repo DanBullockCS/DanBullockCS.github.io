1ae35c881f9ddee7055cc342ef24736e2b111897
DanBullockCS <d.bullock@efg.gg>
2023-11-27 16:07:49 -0500

    Style updates

--- scripts/cardflip.js
+++ scripts/cardflip.js
@@ -3,16 +3,22 @@ var cardDiv;
 
 function flipCard(cardID) {
     cardDiv = document.getElementById(cardID);
-    cardDiv.style.transition = '0.5s';
-    cardDiv.style.transform = 'rotateY(180deg)';
+    cardDiv.style.transition = '0.2s ease-in-out';
+    cardDiv.style.opacity = '0.3';
+    setTimeout(() => {
+        cardDiv.style.opacity = '1';
+    }, 200)
     cardDiv.childNodes[1].style.display = 'none';
     cardDiv.childNodes[3].style.display = 'block';
 }
 
 function flipCardBack(cardID) {
     cardDiv = document.getElementById(cardID);
-    cardDiv.style.transition = '0.5s';
-    cardDiv.style.transform = 'rotateY(360deg)';
+    cardDiv.style.transition = '0.2s ease-in-out';
+    cardDiv.style.opacity = '0.3';
+    setTimeout(() => {
+        cardDiv.style.opacity = '1';
+    }, 200)
     cardDiv.childNodes[1].style.display = 'block';
     cardDiv.childNodes[3].style.display = 'none';
 }
